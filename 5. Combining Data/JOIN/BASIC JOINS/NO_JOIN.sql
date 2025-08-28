f2040752a907b63d4d3cdbd66a8e093c4d92be32
AtunuBiswas <112483046+Atunubiswas@users.noreply.github.com>
2025-08-28 18:58:33 +0530

    Delete 5. Combining Data/JOIN directory

--- 5. Combining Data/JOIN/BASIC JOINS/NO_JOIN.sql
+++
@@ -1,8 +0,0 @@
-/*
-	NO JOIN (return data from two table without combining them'
-*/
-
--- Retrieve all data from customers and orders in two different results 
-
-SELECT * FROM customers;
-SELECT * FROM orders;
